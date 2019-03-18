257d1c6c8913af0c88529d83adb589ac8680f862
clove <1103898149@qq.com>
2019-03-18 12:01:48 +0800

    m

--- user/models.py
+++ user/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=12, verbose_name='用户名')
+    passwd = models.CharField(max_length=512, verbose_name='密码')
+    email = models.EmailField(verbose_name='邮箱地址')
+    permission = models.IntegerField(default=2, verbose_name='用户类型')
+
+class UserInfo(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class HandWrite(models.Model):
+    title = models.CharField(max_length=256, verbose_name='标题')
+    info = models.TextField(verbose_name='作品简介')
+    category_write = models.ForeignKey('CategoryWrite', on_delete=models.CASCADE, verbose_name='书体分类')
+    category_content = models.ForeignKey('CategoryContent', on_delete=models.CASCADE, verbose_name='内容分类')
+    flag = models.BooleanField(default=False, verbose_name='审核情况')
+    score = models.IntegerField(default=0, verbose_name='点赞数')
+    file_path = models.CharField(max_length=256)
+
+class Union(models.Model):
+    name = models.CharField(max_length=12, verbose_name='盟团名称')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='管理者')
+    cover = models.ForeignKey(HandWrite, on_delete=models.CASCADE, verbose_name='封面作品')
+    active = models.IntegerField(default=0, verbose_name='活跃度')
+
+class UnionInfo(models.Model):
+    content = models.TextField()
+    union = models.ForeignKey(Union, on_delete=models.CASCADE)
+
+class CategoryWrite(models.Model):
+    name = models.CharField(max_length=12, verbose_name='书体名称')
+
+class CategoryContent(models.Model):
+    name = models.CharField(max_length=12, verbose_name='内容类别')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+class KnonWrite(models.Model):
+    title = models.CharField(max_length=256, verbose_name='标题')
+    info = models.TextField(verbose_name='作品简介')
+    category_write = models.ForeignKey('CategoryWrite', on_delete=models.CASCADE, verbose_name='书体分类')
+    category_content = models.ForeignKey('CategoryContent', on_delete=models.CASCADE, verbose_name='内容分类')
+    score = models.IntegerField(default=0, verbose_name='点赞数')
