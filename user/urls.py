770e3d2c092c381894bf6b59e628947b261fec93
djx <1181564186@qq.com>
2019-03-19 14:54:32 +0800

    e

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path(r'h/', views.helo, name='helo'),
     path(r'login/', views.login, name='login'),
     path(r'mgr/', views.mgr, name='mgr'),
+    path(r'category_write/', views.category_mgr, name='category_mgr'),
+    path(r'category_write/del/<int:id>/', views.category_write_del),
+    path(r'category_write/add/', views.category_write_add, name='category_write_add'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
-
+from .models import CategoryWrite
+from django.core.paginator import Paginator
+from django.urls import reverse
+from django.forms import ModelForm
 # Create your views here.
 
 def helo(request):
@@ -17,9 +20,36 @@ def mgr(request):
     return render(request, 'mgr.html', {})
 
 
-def umgr(request):
+def user_mgr(request):
     # page = request.GET.get('page', '0')
     pass
 
-def register(request):
 
+def register(request):
+    pass
+
+
+def category_mgr(request):
+    category_writes = CategoryWrite.objects.all()
+    category_writes = Paginator(category_writes, 10).page(1)
+    return render(request, 'category_mgr.html', {'datas': category_writes})
+
+
+def category_write_del(request, id=''):
+    if id:
+        id = int(id)
+        CategoryWrite.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('category_mgr'))
+
+
+class CategoryWriteForm(ModelForm):
+    class Meta:
+        model = CategoryWrite
+        fields = ['name',]
+
+
+def category_write_add(request):
+    form = CategoryWriteForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return HttpResponseRedirect(reverse('category_mgr'))
